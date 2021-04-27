1da3a2d1e20d8a895231c57107a9f6a4aa71aa22
Gregory Prokhorov <grisha.prokhorov03@gmail.com>
2021-04-27 22:16:03 +0300

    catch click

--- src/components/intervalPanel/intervalPanel.jsx
+++ src/components/intervalPanel/intervalPanel.jsx
@@ -3,8 +3,12 @@ import react from "react";
 import "./style.css";
 
 export default function intervalPanel(props) {
+    function handleClick(event) {
+        console.log(event.nativeEvent.offsetX);
+    }
+
     return <div className="container intervalPanel">
-        <div id="musicInterval" className="border"></div>
+        <div onClick={handleClick} id="musicInterval" className="border"></div>
         <div className="controlPanel">
             <button className="submitBth border animation1"></button>
             <button className="submitBth border animation1"></button>
